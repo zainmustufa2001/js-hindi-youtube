20231fad305484d15c785ca156e624078cff0363
ZAIN MUSTAFA <89508092+zainmustufa2001@users.noreply.github.com>
2024-03-13 18:48:49 +0000

    add

---
+++ basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 2189188
+let accountEmail = "zainmustafa499@gmail.com"
+var accountPassword;
+accountCity = "karachi"
+
+ 
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
